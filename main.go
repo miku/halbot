cd0331dca6fdc96e52b003142d0171b1b9c5b7a2
Martin Czygan <martin.czygan@gmail.com>
2015-08-29 14:03:44 +0200

    a basic version for queries

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/danryan/hal"
 	_ "github.com/danryan/hal/adapter/irc"
@@ -9,12 +16,71 @@ import (
 	_ "github.com/danryan/hal/store/memory"
 )
 
-var pingHandler = hal.Hear(`ping`, func(res *hal.Response) error {
-	return res.Send("PONG")
+type Document struct {
+	Title    string `json:"title"`
+	SourceID string `json:"source_id"`
+}
+
+type SolrResponse struct {
+	Response struct {
+		NumFound int        `json:"numFound"`
+		Docs     []Document `json:"docs"`
+	} `json:"response"`
+}
+
+func solrQuery(s string) (SolrResponse, error) {
+	vals := url.Values{}
+	vals.Add("wt", "json")
+	vals.Add("q", s)
+	link := fmt.Sprintf(`%s/select?%s`, os.Getenv("PARROT_SOLR_URL"), vals.Encode())
+	r, err := http.Get(link)
+	if err != nil {
+		return SolrResponse{}, err
+	}
+	defer r.Body.Close()
+	var sr SolrResponse
+	dec := json.NewDecoder(r.Body)
+	err = dec.Decode(&sr)
+	if err != nil {
+		return SolrResponse{}, err
+	}
+	return sr, nil
+}
+
+// queryHandler takes a query and executes it on main site
+var queryHandler = hal.Hear(`hal ai q(\d)? (.+)`, func(res *hal.Response) error {
+	sr, err := solrQuery(res.Match[2])
+
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("%d in AI for %s", sr.Response.NumFound, res.Match[2]))
+
+	if res.Match[1] != "" {
+		size, err := strconv.Atoi(res.Match[1])
+		if err != nil {
+			return err
+		}
+		if size > len(sr.Response.Docs) {
+			size = len(sr.Response.Docs)
+		}
+		if size > 0 {
+			buf.WriteString(" -- ")
+		}
+		var items []string
+		for i := 0; i < size; i++ {
+			doc := sr.Response.Docs[i]
+			items = append(items, fmt.Sprintf("(%d) %s [%s]", i+1, doc.Title, doc.SourceID))
+		}
+		buf.WriteString(strings.Join(items, ", "))
+	}
+
+	return res.Send(buf.String())
 })
 
-var echoHandler = hal.Respond(`echo (.+)`, func(res *hal.Response) error {
-	return res.Reply(res.Match[1])
+var pingHandler = hal.Hear(`ping`, func(res *hal.Response) error {
+	return res.Send("PONG")
 })
 
 func run() int {
@@ -26,7 +92,7 @@ func run() int {
 
 	robot.Handle(
 		pingHandler,
-		echoHandler,
+		queryHandler,
 	)
 
 	if err := robot.Run(); err != nil {
